cdea742629417d0db87f5acb362ba49d816e1c04
rloss <rlossqianss@gmail.com>
2025-05-06 08:14:11 +0900

    814

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-import sys
 import os
+import sys
 from pathlib import Path
 
-# 현재 루트 경로를 PYTHONPATH에 넣기
+# 모듈 경로 인식 보장
 sys.path.append(str(Path(__file__).resolve().parent))
 
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render가 제공하는 포트 사용
+    app.run(host="0.0.0.0", port=port, debug=True)
