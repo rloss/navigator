c693d514acaca56de01dd1ea86e74c7b1d7ea1cb
rloss <rlossqianss@gmail.com>
2025-05-06 09:40:02 +0900

    디자인

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-DATABASE_URL=postgresql://navigator_owner:npg_j9NHeIuoRW6G@ep-wild-cloud-a1gkpmtv-pooler.ap-southeast-1.aws.neon.tech/navigator?sslmode=require
-SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+OPENAI_API_KEY = 
+DATABASE_URL= postgresql://navigator_owner:npg_j9NHeIuoRW6G@ep-wild-cloud-a1gkpmtv-pooler.ap-southeast-1.aws.neon.tech/navigator?sslmode=require
+SUPABASE_KEY = 
