7249f7b258d6afb5816509f0d9fe2d0e569b83b7
rloss <rlossqianss@gmail.com>
2025-05-06 09:24:52 +0900

    리콰이어수정

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-SUPABASE_URL = os.getenv('SUPABASE_URL')
+DATABASE_URL=postgresql://navigator_owner:npg_j9NHeIuoRW6G@ep-wild-cloud-a1gkpmtv-pooler.ap-southeast-1.aws.neon.tech/navigator?sslmode=require
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
