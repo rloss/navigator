1b191ac8dfefddd258b45b4ff8b4e9ba9aa18f9f
rloss <rlossqianss@gmail.com>
2025-05-06 09:45:47 +0900

    디자인디자인

--- app/views/home.py
+++ app/views/home.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
+from app.models import Trend, StrategyCard
 
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
 def home():
-    return render_template("home.html")
+    trends = Trend.query.order_by(Trend.id.desc()).all()
+    strategies = StrategyCard.query.order_by(StrategyCard.id.desc()).all()
+    return render_template("home.html", trends=trends, strategies=strategies)
